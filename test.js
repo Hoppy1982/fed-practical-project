7e0a2fcccb62a0acf5a2af4c24fca2ab3d184bf8
mark hopcraft <mark82hopcraft@gmail.com>
2019-03-01 10:56:29 +0000

    removed margin-left: 0 from form, must've been a mistake

---
+++ test.js
@@ -0,0 +1,21 @@
+var a = 10
+let b = 1
+const c = 3
+const ACCOUNT_NUMBER = 234324
+
+function myFunc() {
+  for (let i=0; i<10; i++) {
+
+  }
+
+  console.log(i)
+}
+
+
+let myCar95 = {
+  colour: 'red',
+  this.wheels: 4,
+  drive: function() {
+    console.log('Driving' + this.wheels)
+  }
+}
